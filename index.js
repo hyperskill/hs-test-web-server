d8be74e458733b193736129ab048a1310e29e050
aaaaaa2493 <aaaaaa2493@yandex.ru>
2020-04-29 21:41:39 +0300

    Refactor index.js

--- index.js
+++ index.js
@@ -1,67 +1,17 @@
-// @ts-check
-'use strict'
+const execa = require('execa');
+const waitOn = require('wait-on');
 
-const execa = require('execa')
-const waitOn = require('wait-on')
-const Promise = require('bluebird')
-const psTree = require('ps-tree')
+async function startServerAndTest(host, port, test, path) {
+    let node = `"${process.argv[0]}" "${__dirname}/start.js"`;
+    let params = `host:${host} port:${port} "path:${path}"`;
+    const server = execa(`${node} ${params}`);
 
-module.exports = ({service, runTests}) => {
-  let url = service.url;
-  let start = `node "${__dirname}/start.js"`
-  const server = execa(`${start} port:${service.port} host:${service.host}`, { shell: true, stdio: 'inherit' })
-  let serverStopped
-
-  function stopServer () {
-    if (!serverStopped) {
-      serverStopped = true
-      return Promise.fromNode(cb => psTree(server.pid, cb))
-        .then(children => {
-          children.forEach(child => {
-            try {
-              process.kill(child.PID, 'SIGINT')
-            } catch (e) {
-              if (e.code === 'ESRCH') {
-                console.log(
-                  `Child process ${child.PID} exited before trying to stop it`
-                )
-              } else {
-                throw e
-              }
-            }
-          })
-        })
-        .then(() => {
-          server.kill()
-        })
+    try {
+        await waitOn({resources: [`http://${host}:${port}`], window: 1000});
+        return await test();
+    } finally {
+        server.kill();
     }
-  }
-
-  const waited = new Promise((resolve, reject) => {
-    const onClose = () => {
-      reject(new Error('server closed unexpectedly'))
-    }
-
-    server.on('close', onClose)
-
-    const options = {
-      resources: Array.isArray(url) ? url : [url],
-      interval: 2000,
-      window: 1000
-    }
-
-    waitOn(options, err => {
-      if (err) {
-        return reject(err)
-      }
-      server.removeListener('close', onClose)
-      resolve()
-    })
-  })
-
-  return waited
-    .tapCatch(stopServer)
-    .then(runTests)
-    .finally(stopServer)
 }
 
+exports.startServerAndTest = startServerAndTest;
