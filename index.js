b20fa1a28cc74d6e06b9e467b01623d09d3f0689
aaaaaa2493 <aaaaaa2493@yandex.ru>
2020-04-30 03:26:03 +0300

    Exeriment lightweight webpack

--- index.js
+++ index.js
@@ -1,16 +1,64 @@
-const execa = require('execa');
-const waitOn = require('wait-on');
+const path = require('path');
 
-async function startServerAndTest(host, port, path, test) {
-    let node = `"${process.argv[0]}" "${__dirname}/start.js"`;
-    let params = `host:${host} port:${port} "path:${path}"`;
-    const server = execa(`${node} ${params}`);
+const Webpack = require('webpack');
+const WebpackDevServer = require('webpack-dev-server');
+
+function runServer(host, port, src) {
+    const webpackConfig = {
+        mode: 'development',
+        entry: './' + src + 'index.js',
+        module: {
+            rules: [
+                {
+                    test: new RegExp(src + '.*\.js$'),
+                    loader: 'babel-loader',
+                    options: {
+                        babelrc: false,
+                        configFile: false,
+                        presets: ["@babel/preset-react"],
+                        cacheDirectory: true,
+                        cacheCompression: false,
+                        compact: false,
+                    },
+                },
+                {
+                    test: new RegExp(src + '.*\.css$'),
+                    use: ['style-loader', 'css-loader']
+                },
+                {
+                    test: new RegExp(src + '.*\.(png|svg|jpg|gif)$'),
+                    use: 'file-loader'
+                },
+                {
+                    test: new RegExp(src + '.*\.(woff|woff2|eot|ttf|otf)$'),
+                    use: 'file-loader'
+                },
+            ]
+        },
+        devtool: 'cheap-module-eval-source-map',
+        devServer: {
+            contentBase: path.join(process.cwd(), 'public')
+        }
+    };
+
+    const compiler = Webpack(webpackConfig);
+    const server = new WebpackDevServer(compiler, webpackConfig.devServer);
+
+    server.listen(port, host, () => {
+        console.log(`Starting server on http://${host}:${port}`);
+    });
+
+    return server;
+}
+
+async function startServerAndTest(host, port, stage, test) {
+    const src = stage.substring(process.cwd().length + 1) + '/src/';
+    const server = runServer(host, port, src);
 
     try {
-        await waitOn({resources: [`http://${host}:${port}`], window: 1000});
         return await test();
     } finally {
-        server.kill();
+        server.close();
     }
 }
 
