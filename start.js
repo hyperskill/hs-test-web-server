3c54bfec57485bf24093adfc8252789cc07fe1f1
aaaaaa2493 <aaaaaa2493@yandex.ru>
2020-06-10 17:53:44 +0300

    Support class properties syntax

--- start.js
+++ start.js
@@ -27,7 +27,13 @@ function runServer(host, port, src) {
                     loader: 'babel-loader',
                     exclude: /node_modules/,
                     options: {
-                        presets: ["@babel/preset-env", "@babel/preset-react"]
+                        presets: [
+                            "@babel/preset-env", 
+                            "@babel/preset-react"
+                        ],
+                        plugins: [
+                            ["@babel/plugin-proposal-class-properties", { "loose": true }]
+                        ]
                     }
                 },
                 {
